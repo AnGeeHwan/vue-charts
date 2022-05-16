b32db4cda14d6d1ade1d14153dadcdd9d0e38302
AGH <rlghks9252@gmail.com>
2022-05-16 17:34:26 +0900

    Use chart.js

---
+++ chart-lib/src/components/BarChart.vue
@@ -0,0 +1,30 @@
+<template>
+  <canvas ref="barChart" id="myBarChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+  props: ["propsdata"],
+  mounted() {
+    // const ctx = document.getElementById("myBarChart");
+    const ctx = this.$refs.barChart;
+    const myBarChart = new this.$_Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: this.propsdata,
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+    console.log(myBarChart);
+  },
+};
+</script>
+
+<style></style>
